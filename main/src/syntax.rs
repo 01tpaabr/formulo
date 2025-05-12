dc95a560954609c5a6024696568685c7393fa32b
Thiago Andrade <01tpaa.br@gmail.com>
2025-05-11 22:55:55 -0300

    refactor syntax node

--- main/src/syntax.rs
+++ main/src/syntax.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::fmt;
 
 // Symbols to glue together less complex formulas
@@ -6,7 +7,8 @@ pub enum Operator {
     And,
     Or,
     Imp,
-    Bimp
+    Bimp,
+    Atomic
 }
 
 pub enum OperatorArity {
@@ -14,6 +16,7 @@ pub enum OperatorArity {
     Binary
 }
 
+// Ideally if new operator is to be added, the only changes that are need are in this section, along with adding one Invariant in Wff enum
 impl Operator {
     pub fn arity(operator: &Operator) -> OperatorArity {
         match operator {
@@ -24,6 +27,9 @@ impl Operator {
 
     pub fn operator_repr(operator: &Operator) -> &str {
         match operator {
+            Self::Atomic => {
+                ""
+            }
             Self::Not => {
                 "~"
             }
@@ -38,6 +44,29 @@ impl Operator {
             }
             Self::Bimp => {
                 "<->"
+            }
+        }
+    }
+
+    pub fn build_formula_type_by_operator(operator: &Operator, repr: String) -> Wff {
+        match operator {
+            Operator::Atomic => {
+                Wff::BimpFormula(repr)
+            }
+            Operator::Not => {
+                Wff::NotFormula(repr)
+            }
+            Operator::And => {
+                Wff::AndFormula(repr)
+            }
+            Operator::Or => {
+                Wff::OrFormula(repr)
+            }
+            Operator::Imp => {
+                Wff::ImpFormula(repr)
+            }
+            Operator::Bimp => {
+                Wff::BimpFormula(repr)
             }
         }
     }
@@ -69,6 +98,7 @@ impl Operator {
 }
 
 // wff separated by their main connective
+#[derive(Clone)]
 pub enum Wff {
     Atomic(String),
     NotFormula(String),
@@ -84,8 +114,30 @@ impl fmt::Display for Wff {
     }
 }
 
-
 impl Wff {
+    pub fn formula_type_to_operator(wff: &Wff) -> Operator {
+        match wff {
+            Wff::Atomic(_) => {
+                Operator::Atomic
+            }
+            Wff::NotFormula(_) => {
+                Operator::Not
+            }
+            Wff::AndFormula(_) => {
+                Operator::And
+            }
+            Wff::OrFormula(_) => {
+                Operator::Or
+            }
+            Wff::ImpFormula(_) => {
+                Operator::Imp
+            }
+            Wff::BimpFormula(_) => {
+                Operator::Bimp
+            }
+        }
+    }
+
     pub fn get_formula_repr(&self) -> String{
         match self {
             Wff::Atomic(s)
@@ -101,37 +153,17 @@ impl Wff {
         Wff::Atomic(placeholder.to_string())
     }
 
-    pub fn operator_formula_type(operator: &Operator, repr: String) -> Wff {
-        match operator {
-            Operator::Not => {
-                Wff::NotFormula(repr)
-            }
-            Operator::And => {
-                Wff::AndFormula(repr)
-            }
-            Operator::Or => {
-                Wff::OrFormula(repr)
-            }
-            Operator::Imp => {
-                Wff::ImpFormula(repr)
-            }
-            Operator::Bimp => {
-                Wff::BimpFormula(repr)
-            }
-        }
-    }
-
     pub fn build_wff(operator: Operator, left_formula: Option<&Wff>, right_formula: Option<&Wff>) -> Wff {
         let op_arity: OperatorArity = Operator::arity(&operator);
 
         match op_arity {
             OperatorArity::Unary => {
                 match (left_formula, right_formula) {
-                    (None, None) => panic!("Wff building erorr (None, None)"),
+                    (None, None) => panic!("Wff building error (None, None)"),
                     (None, _) => panic!("Wff building error (None, _)"),
                     (Some(left_repr), _) => {
                         let repr_string = Operator::wrap_operator(&operator, Some(left_repr.get_formula_repr()), None);
-                        Wff::operator_formula_type(&operator, repr_string)
+                        Operator::build_formula_type_by_operator(&operator, repr_string)
                     }
                 }
             }
@@ -139,14 +171,65 @@ impl Wff {
                 match (left_formula, right_formula) {
                     (Some(left_repr), Some(right_repr)) => {
                         let repr_string = Operator::wrap_operator(&operator, Some(left_repr.get_formula_repr()), Some(right_repr.get_formula_repr()));
-                        Wff::operator_formula_type(&operator, repr_string)
+                        Operator::build_formula_type_by_operator(&operator, repr_string)
                     }
                     (_, _) => panic!("Operator wrapping error not (Some(), Some())"),
                 }
             }
         }
-    
     }
 
+}
 
+#[derive(Clone)]
+pub struct SyntaxNode {
+    content: Wff,
+    left: Option<Box<SyntaxNode>>,
+    right: Option<Box<SyntaxNode>>
+}
+
+impl SyntaxNode {
+    pub fn get_wff(&self) -> Wff{
+        self.content.clone()
+    }
+
+    pub fn build_syntax_node(operator: Operator, left_formula: Option<Box<SyntaxNode>>, right_formula: Option<Box<SyntaxNode>>) -> SyntaxNode {
+        let op_arity: OperatorArity = Operator::arity(&operator);
+
+        match op_arity {
+            OperatorArity::Unary => {
+                match (left_formula, right_formula) {
+                    (None, None) => panic!("Wff building error (None, None)"),
+                    (None, _) => panic!("Wff building error (None, _)"),
+                    (Some(left_box), _) => {
+                        let repr_string = Operator::wrap_operator(&operator, Some(left_box.get_wff().get_formula_repr()), None);
+                        let new_wff = Operator::build_formula_type_by_operator(&operator, repr_string);
+                        SyntaxNode{
+                            content: new_wff,
+                            left: Some(left_box.clone()),
+                            right: None
+                        }
+                    }
+                }
+            }
+            OperatorArity::Binary => {
+                match (left_formula, right_formula) {
+                    (Some(left_box), Some(right_box)) => {
+                        let repr_string = Operator::wrap_operator(&operator, Some(left_box.get_wff().get_formula_repr()), Some(right_box.get_wff().get_formula_repr()));
+                        let new_wff = Operator::build_formula_type_by_operator(&operator, repr_string);
+                        SyntaxNode{
+                            content: new_wff,
+                            left: Some(left_box.clone()),
+                            right: Some(right_box.clone())
+                        }
+                    }
+                    (_, _) => panic!("Operator wrapping error not (Some(), Some())"),
+                }
+            }
+        }
+    }
+
+    pub fn parse_string_into_syntax_node(input_string : String) -> SyntaxNode {
+        panic!()
+    }
 }
